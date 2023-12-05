041a4e8ce8b35f6fd5ef0ffea9143bea9c285580
pthrr <pthrr@posteo.de>
2023-12-05 13:35:09 +0100

    Updated deps
    What:
    
    Why:
    
    How:
    
    Tags:

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -1,9 +1,14 @@
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
 
-#include "../src/print.hpp"
-
-TEST( basic, print )
+unsigned int Factorial( unsigned int number )
 {
-    auto rval = print( "My message" );
-    ASSERT_TRUE( rval == 0 );
+    return number <= 1 ? number : Factorial( number - 1 ) * number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" )
+{
+    REQUIRE( Factorial( 1 ) == 1 );
+    REQUIRE( Factorial( 2 ) == 2 );
+    REQUIRE( Factorial( 3 ) == 6 );
+    REQUIRE( Factorial( 10 ) == 3628800 );
 }
